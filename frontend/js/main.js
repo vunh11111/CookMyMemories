081f29afe7d64eea55c5b796c1bc6858885767d1
Pucci1015 <192977798+Pucci1015@users.noreply.github.com>
2025-08-08 09:53:41 +0700

    Add files via upload

---
+++ frontend/js/main.js
@@ -0,0 +1,53 @@
+/***** main *****/
+let main = document.getElementsByTagName("main")[0];
+
+//main top
+let mainTop = document.createElement("div");
+mainTop.classList.add("main_top");
+main.appendChild(mainTop);
+
+let mainTopImg = document.createElement("img");
+mainTopImg.src = "/img/main_top.png";
+mainTop.appendChild(mainTopImg);
+
+let mainTopText = document.createElement("p");
+mainTopText.innerHTML = "あい<br>うえお";
+mainTop.appendChild(mainTopText);
+
+//main content
+let mainContentTitle = document.createElement("p");
+mainContentTitle.classList.add("main_content_title");
+mainContentTitle.innerHTML = "abc";
+main.appendChild(mainContentTitle);
+
+let mainContent = document.createElement("p");
+mainContent.classList.add("main_content");
+mainContent.innerHTML = "abc";
+main.appendChild(mainContent);
+
+// main menu
+let mainMenuDiv = document.createElement("div");
+mainMenuDiv.classList.add("main_menu");
+main.appendChild(mainMenuDiv);
+
+for ( var i = 0 ; i < menuList.length ; i++ ) {
+    if ( menuList[i].type === 'search' ) {
+        let mainMenuLink = document.createElement("a");
+        mainMenuLink.href = "/" + menuList[i].link;
+        mainMenuDiv.appendChild(mainMenuLink);
+
+        let mainMenuImg = document.createElement("div");
+        mainMenuImg.classList.add("main_menu_img")
+        mainMenuLink.appendChild(mainMenuImg);
+
+        let imgName = "url(\"/img/" + menuList[i].name + ".svg\")";
+        mainMenuImg.style.maskImage = imgName;
+        mainMenuImg.style.setProperty('-webkit-mask-image',imgName);
+        mainMenuImg.style.WebkitMaskImage = imgName;
+
+        let mainMenuTitle = document.createElement("p");
+        mainMenuTitle.innerHTML = menuList[i].title;
+        mainMenuLink.appendChild(mainMenuTitle);
+    }
+}
+
---
+++ frontend/result/js/main.js
@@ -0,0 +1,75 @@
+/***** main *****/
+let main = document.getElementsByTagName("main")[0];
+
+//breadcrumbs
+breadcrumbsFn(main);
+
+//main title
+mainTitleFn(main,menuList[2].title);
+
+//cooking obj (sample)
+let cooking = {
+    sushi: {
+        name: "寿司",
+        material: [ "米" , "酢" , "マグロ" ],
+        image: "img/sushi_tuna.png",
+        way: [
+            "米を炊く",
+            "酢と米を混ぜる"
+        ]
+    },
+    onigiri: {
+        name: "おにぎり",
+        material: [ "米" , "のり" , "梅" ],
+        image: "img/sushi_tuna.png",
+        way: [
+            "米を炊く",
+            "にぎる"
+        ]
+    }
+}
+
+//result
+let resultDiv = document.createElement("div");
+resultDiv.classList.add("result");
+main.appendChild(resultDiv);
+
+const resultDisplay = (resultObj) => {
+    let resultLink = document.createElement("a");
+    resultLink.href = "#";
+    resultDiv.appendChild(resultLink);
+
+    let result = document.createElement("div");
+    result.classList.add("result_element");
+    resultLink.appendChild(result);
+
+    let resultImg = document.createElement("img");
+    resultImg.src = resultObj.image;
+    result.appendChild(resultImg);
+
+    let resultTitle = document.createElement("p");
+    resultTitle.innerHTML = resultObj.name;
+    resultTitle.classList.add("result_title");
+    result.appendChild(resultTitle);
+
+    let resultMaterial = document.createElement("p");
+    let material = "材料: ";
+    for ( var i = 0 ; i < resultObj.material.length ; i++ ) {
+        material += resultObj.material[i];
+        if ( i !== resultObj.material.length - 1 ) material += " , ";
+    }
+    resultMaterial.innerHTML = material;
+    resultMaterial.classList.add("result_material");
+    result.appendChild(resultMaterial);
+};
+
+for ( var i = 0 ; i < Object.values(cooking).length ; i++ ) resultDisplay(Object.values(cooking)[i]);
+
+
+
+
+
+
+
+
+
